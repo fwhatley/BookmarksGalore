15e6b04ffd2680eb0b0a625f5edf867f91360960
Fredy Whatley (rw407x) <rw407x@att.com>
2020-02-16 01:01:39 -0500

    add firebase details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCkiFtMqVQwpnIjygeeCDE2W4AglIsGoLQ',
+    authDomain: 'bookmarksgalore.firebaseapp.com',
+    databaseURL: 'https://bookmarksgalore.firebaseio.com',
+    projectId: 'bookmarksgalore',
+    storageBucket: 'bookmarksgalore.appspot.com',
+    messagingSenderId: '1573754782',
+    appId: '1:1573754782:web:9b8a73055837961682688f',
+    measurementId: 'G-18M3R4LZ8Q'
+  }
 };
